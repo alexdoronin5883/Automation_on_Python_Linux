91e28156e79e15d3b04c9a44b3e7627047b8415b
Александр Доронин <107746307+alexdoronin5883@users.noreply.github.com>
2023-12-08 16:36:44 +0300

    Update deploy.py

--- Pytest_project/deploy.py
+++ Pytest_project/deploy.py
@@ -3,10 +3,10 @@ from sshcheckers import ssh_checkout, upload_files
 
 def deploy():
     res = []
-    upload_files("192.168.0.109", "vboxuser", "alex5883", "tests/p7zip-full.deb", "/home/vboxuser/p7zip-full.deb")
-    res.append(ssh_checkout("192.168.0.109", "vboxuser", "alex5883", "echo 'alex5883' | sudo -S dpkg -i /home/vboxuser/p7zip-full.deb",
+    upload_files("xxx.xxx.x.xxx", "nameuser", "xxxxxxxx", "tests/p7zip-full.deb", "/home/nameuser/p7zip-full.deb")
+    res.append(ssh_checkout("xxx.xxx.x.xxx", "nameuser", "xxxxxxxx", "echo 'xxxxxxxx' | sudo -S dpkg -i /home/nameuser/p7zip-full.deb",
                             "Настраивается пакет"))
-    res.append(ssh_checkout("192.168.0.109", "vboxuser", "alex5883", "echo 'alex5883' | sudo -S dpkg -s p7zip-full",
+    res.append(ssh_checkout("xxx.xxx.x.xxx", "nameuser", "xxxxxxxx", "echo 'xxxxxxxx' | sudo -S dpkg -s p7zip-full",
                             "Status: install ok installe"))
     return all(res)
 
